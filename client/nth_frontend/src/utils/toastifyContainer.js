b889e41fc8955ce1180fb1f860d7fac281dba86f
VanshTeppalwar <vanshteppalwar@gmail.com>
2022-09-25 13:33:59 +0530

    react toast style

--- client/nth_frontend/src/utils/toastifyContainer.js
+++ client/nth_frontend/src/utils/toastifyContainer.js
@@ -1,18 +1,20 @@
 import { ToastContainer, toast } from "react-toastify";
-
+import './toastifyContainer.css';
 export const ToastUtils = () => {
     return (
         <>
             <ToastContainer
                 position="top-right"
                 autoClose={3000}
-                hideProgressBar={false}
+                // hideProgressBar={false}
+                hideProgressBar
                 newestOnTop={false}
                 closeOnClick
                 draggable
-                pauseOnHover
-                theme="dark"
-                toastStyle={{ backgroundColor: "black" }}    
+                // pauseOnHover
+                // theme="dark"
+                style={{fontSize:'1px'}}    
+                toastClassName="nes-container is-dark is-rounded"
             />
         </>
     )
