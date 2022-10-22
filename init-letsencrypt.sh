f646ef82be426d854a53e03bc0324a8c2b8e601a
Devraj Shetake <devrajshetake@gmail.com>
2022-10-22 10:50:06 +0530

    domain changed

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(nth.pictieee.in www.nth.pictieee.in)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="naphade21@gail.com" # Adding a valid address is strongly recommended
+email="devrajshetake@gmail.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
