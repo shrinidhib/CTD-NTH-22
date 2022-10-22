0755e34a639aa3503c926ddcb37cffadfef83e90
Devraj Shetake <devrajshetake@gmail.com>
2022-10-22 15:25:22 +0530

    removed www

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(admin.nth.pictieee.in www.admin.nth.pictieee.in)
+domains=(admin.nth.pictieee.in)
 rsa_key_size=4096
 data_path="./nginx/certbot"
 email="devrajshetake@gmail.com" # Adding a valid address is strongly recommended
