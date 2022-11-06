b09ac18bea6fa9a8a4f03b275fddee7133543508
VanshTeppalwar <vanshteppalwar@gmail.com>
2022-11-06 15:06:58 +0530

    Contact Page

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -4,49 +4,60 @@ import devraj from '../../assets/team/devraj.png';
 import harsh from '../../assets/team/harsh.png';
 import bhakti from '../../assets/team/bhakti.png';
 import Requests from '../../api/requests';
-import {useState,useEffect} from 'react';
+import { useState, useEffect } from 'react';
 const Contact = () => {
-  const [loaderStatus, setLoaderStatus] = useState(false);
+    const [loaderStatus, setLoaderStatus] = useState(false);
     const eventStatus = async () => {
         await Requests.time()
-        .catch ((err)=> {
-          console.log(err);
-          setLoaderStatus(true);
-        })
-      }
+            .catch((err) => {
+                console.log(err);
+                setLoaderStatus(true);
+            })
+    }
     const details = [
         {
             name: "Devraj Shetake",
-            img : devraj,
+            img: devraj,
             phone: 8208566305
         },
         {
             name: "Harsh Bhat",
-            img : harsh,
+            img: harsh,
             phone: 8983594252
         },
         {
             name: "Bhakti Mahurkar",
-            img : bhakti,
-            phone: 9307931414 
+            img: bhakti,
+            phone: 9307931414
         }
     ]
-    useEffect(()=>{
+    useEffect(() => {
         eventStatus();
-    },[])
+    }, [])
     return (
         <div className='contact-page'>
 
             {/* <h1 className='topic-title'>Web Team</h1> */}
             <Container >
-            
+
                 <Row>
-                    
                     {
-                    details.map((data) => (
-                    
-                    <Col lg={4} sm={12}>
-                        {/* <section class="nes-container is-dark member-card">
+                        loaderStatus
+                            ?
+                            <div>
+                                <p style={{ color: 'white' }}><p style={{ color: 'red', display: 'contents' }}>To resolve the Network Error:</p> On Chrome, click on 3 dots --&gt; settings --&gt; privacy and security --&gt; use secure dns --&gt; choose another provider --&gt; select Google (public DNS)
+                                </p>
+                                <p style={{ color: 'white', 'text-decoration': 'underline' }}>If the network error persists, contact any of the organizers.
+                                </p>
+                            </div>
+                            :
+                            <></>
+                    }
+                    {
+                        details.map((data) => (
+
+                            <Col lg={4} sm={12}>
+                                {/* <section class="nes-container is-dark member-card">
                             <div class="avatar">
                                 <img data-src={data.img} alt="Core Member B.C.Rikko" class="" src={data.img}></img>
                             </div>
@@ -64,31 +75,20 @@ const Contact = () => {
                                 <a href="tel:+91 94033 04652"><i class="fas fa-phone-alt"></i> +91 94033 04652</a>
                             </div>
                         </section> */}
-                        <section class="nes-container is-dark member-card m-5">
-                        <div class="contactus-card"><img src={data.img} width="210px" alt='profile'></img>
-                            <div class="card-name">
-                                <h4 class="text">{data.name}</h4>
-                                <h5 ><a href={`tel:+91 ${data.phone}`} style={{color:'blue'}} class="text1"><i class="fas fa-phone-alt"></i> {`tel:+91 ${data.phone}`}</a></h5>
-                            </div>
-                        </div>
-                        </section>
-                    </Col>
-                    ))}
-                    {
-                        loaderStatus
-                        ?
-                        <div>
-                        <p style={{color:'white'}}><p style={{color:'red',display:'contents'}}>To resolve the Network Error:</p> On Chrome, click on 3 dots --&gt; settings --&gt; privacy and security --&gt; use secure dns --&gt; choose another provider --&gt; select Google (public DNS)
-                        </p>    
-                        <p style={{color:'white','text-decoration': 'underline'}}>If the network error persists, contact any of the organizers.
-                        </p>
-                        </div>
-                        :
-                        <></>
-                    }
+                                <section class="nes-container is-dark member-card m-5">
+                                    <div class="contactus-card"><img src={data.img} width="210px" alt='profile'></img>
+                                        <div class="card-name">
+                                            <h4 class="text">{data.name}</h4>
+                                            <h5 ><a href={`tel:+91 ${data.phone}`} style={{ color: 'blue' }} class="text1"><i class="fas fa-phone-alt"></i> {`tel:+91 ${data.phone}`}</a></h5>
+                                        </div>
+                                    </div>
+                                </section>
+                            </Col>
+                        ))}
+
                 </Row>
             </Container>
-            
+
         </div>
     )
 }
