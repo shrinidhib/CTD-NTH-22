8e8e0bfe781e6665d08bbc3a1d165d8a5dae8a23
kshitijdhapse <kshitijdhapse@gmail.com>
2024-03-25 01:07:45 +0530

    members

--- client/src/pages/Team/Team.js
+++ client/src/pages/Team/Team.js
@@ -3,39 +3,34 @@ import './Team.css'
 const Team = () => {
     const webteam = [
         {
-            name: 'Vansh Teppalwar',
+            name: 'Kshitij',
+            img: 'https://i.postimg.cc/T1GPFsTt/kshitij1.jpg',
+            github: 'https://github.com/KshitijDhapse',
+            linkedin: 'https://www.linkedin.com/in/kshitijdhapse/'
+        },
+        {
+            name: 'Karan',
+            img: 'https://i.postimg.cc/9DLjkLbn/Karan.jpg',
+            github: 'https://github.com/VanshTeppalwar',
+            linkedin: 'https://www.linkedin.com/in/karan-mundhada-075b071b7/'
+        },
+        {
+            name: 'Hitesh',
             img: 'https://github.com/BcRikko.png?size=80',
             github: 'https://github.com/VanshTeppalwar',
             linkedin: 'https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/'
         },
         {
-            name: 'Devraj',
-            img: 'https://github.com/BcRikko.png?size=80',
+            name: 'Atharva',
+            img: 'https://i.postimg.cc/JsrLKfNX/Atharva-Date.jpg',
             github: 'https://github.com/VanshTeppalwar',
-            linkedin: 'https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/'
+            linkedin: 'https://www.linkedin.com/in/atharva-date-04345b256/'
         },
         {
-            name: 'Harsh',
-            img: 'https://github.com/BcRikko.png?size=80',
+            name: 'Riddhi',
+            img: 'https://i.postimg.cc/c630JM1v/Riddhi-Kulkarni.jpg',
             github: 'https://github.com/VanshTeppalwar',
-            linkedin: 'https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/'
-        },
-        {
-            name: 'Mrunal',
-            img: 'https://github.com/BcRikko.png?size=80',
-            github: 'https://github.com/VanshTeppalwar',
-            linkedin: 'https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/'
-        },
-        {
-            name: 'Bhakti',
-            img: 'https://github.com/BcRikko.png?size=80',
-            github: 'https://github.com/VanshTeppalwar',
-            linkedin: 'https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/'
-        },{
-            name: 'Omkar',
-            img: 'https://github.com/BcRikko.png?size=80',
-            github: 'https://github.com/VanshTeppalwar',
-            linkedin: 'https://www.linkedin.com/in/vansh-teppalwar-0b60b0217/'
+            linkedin: 'https://www.linkedin.com/in/riddhi-kulkarni-9a6b84232/'
         },
     ]
     return (
@@ -70,12 +65,24 @@ const Team = () => {
             
             
             <h1 className='topic-title'>NTH-TEAM</h1>
-            
+            {/* <div style="display: flex; justify-content: center;"> */}
             <a href="https://github.com/4k1k0" target="_black" class="contributor">
-            <img data-src="https://github.com/4k1k0.png?size=64" alt="Contributor 4k1k0" class="nes-avatar is-large is-rounded" src="https://github.com/4k1k0.png?size=64"></img>
-            <p>Devraj</p>
+            <img data-src="https://i.postimg.cc/Pv1jFhVW/Jobin.jpg" alt="Contributor 4k1k0" class="nes-avatar is-large is-rounded" src="https://i.postimg.cc/Pv1jFhVW/Jobin.jpg"></img>
+            <p>Jobin</p>
             </a>
-
+            <a href="https://github.com/4k1k0" target="_black" class="contributor">
+            <img data-src="https://i.postimg.cc/PpwsPTjg/Samir.jpg" alt="Contributor 4k1k0" class="nes-avatar is-large is-rounded" src="https://i.postimg.cc/PpwsPTjg/Samir.jpg"></img>
+            <p>Samir</p>
+            </a>
+            <a href="https://github.com/4k1k0" target="_black" class="contributor">
+            <img data-src="https://i.postimg.cc/N57t6sHY/Samruddhi.jpg" alt="Contributor 4k1k0" class="nes-avatar is-large is-rounded" src="https://i.postimg.cc/N57t6sHY/Samruddhi.jpg"></img>
+            <p>Samruddhi</p>
+            </a>
+            <a href="https://github.com/4k1k0" target="_black" class="contributor">
+            <img data-src="https://i.postimg.cc/dLKvZkD4/Srinidhi1.jpg" alt="Contributor 4k1k0" class="nes-avatar is-large is-rounded" src="https://i.postimg.cc/dLKvZkD4/Srinidhi1.jpg"></img>
+            <p>Srinidhi</p>
+            </a>
+            {/* </div> */}
         </div>
     )
 }
