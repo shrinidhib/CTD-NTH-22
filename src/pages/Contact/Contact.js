27c2672dce9f635f4c9607df060a262316fb2f45
kshitijdhapse <kshitijdhapse@gmail.com>
2024-04-10 13:19:08 +0530

    contacts

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -8,62 +8,42 @@ const Contact = () => {
             img: 'https://i.postimg.cc/9DLjkLbn/Karan.jpg',
             github: 'https://github.com/Karan-mundhada',
             linkedin: 'https://www.linkedin.com/in/karan-mundhada-075b071b7/',
-            phone: 8080792381
+            phone: '8080792381' // Make sure phone number is a string
         },
         {
             name: 'Kshitij Dhapse',
             img: 'https://i.postimg.cc/T1GPFsTt/kshitij1.jpg',
             instagram: 'https://www.instagram.com/kshitij_dhapse/',
             linkedin: 'https://www.linkedin.com/in/kshitijdhapse/',
-            phone: 9325815244
-
+            phone: '9325815244' // Make sure phone number is a string
         }
-    ]
+    ];
+
     return (
         <div className='contact-page'>
-
-            {/* <h1 className='topic-title'>Web Team</h1> */}
-            <Container >
-            
+            <Container>
                 <Row>
-                    
-                    {
-                    details.map((data) => (
-                    
-                    <Col lg={6} sm={12}>
-                        {/* <section class="nes-container is-dark member-card">
-                            <div class="avatar">
-                                <img data-src={data.img} alt="Core Member B.C.Rikko" class="" src={data.img}></img>
-                            </div>
-                            {console.log(data)}
-                            <div class="profile">
-                                <h4 class="name">{data.name}</h4>
-                                <div>
-                                    <a href={data.instagram} target="_blank" rel="noopener" aria-label="github">
-                                        <i class="nes-icon instagram"></i>
-                                    </a>
-                                    <a href={data.linkedin} target="_blank" rel="noopener" aria-label="linkedin">
-                                        <i class="nes-icon linkedin"></i>
-                                    </a>
+                    {details.map((data) => (
+                        <Col lg={6} sm={12}>
+                            <section class="nes-container is-dark member-card m-5">
+                                <div class="contactus-card">
+                                    <img src={data.img} width="210px"></img>
+                                    <div class="card-name">
+                                        <h4 class="text">{data.name}</h4>
+                                        <h5>
+                                            <a href={`tel:+91${data.phone}`} style={{ color: 'blue' }} class="text1">
+                                                <i class="fas fa-phone-alt"></i> {data.phone}
+                                            </a>
+                                        </h5>
+                                    </div>
                                 </div>
-                                <a href="tel:+91 94033 04652"><i class="fas fa-phone-alt"></i> +91 94033 04652</a>
-                            </div>
-                        </section> */}
-                        <section class="nes-container is-dark member-card m-5">
-                        <div class="contactus-card"><img src={data.img} width="210px"></img>
-                            <div class="card-name">
-                                <h4 class="text">{data.name}</h4>
-                                <h5 ><a href="tel: {data.phone}" style={{color:'blue'}} class="text1"><i class="fas fa-phone-alt"></i> {data.phone}</a></h5>
-                            </div>
-                        </div>
-                        </section>
-                    </Col>
+                            </section>
+                        </Col>
                     ))}
                 </Row>
             </Container>
-            
         </div>
-    )
-}
+    );
+};
 
 export default Contact;
